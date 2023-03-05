c317657484c981a3fe9b65e00952ed5d12ca5650
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-05 12:32:20 +0100

    add providers and poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,38 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import NiceModal from "@ebay/nice-modal-react";
+import { ToastContainer } from "react-toastify";
+import { Poppins } from "@next/font/google";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+const poppins = Poppins({
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Kwik App</title>
+      </Head>
+      <SessionProvider session={session}>
+        <NiceModal.Provider>
+          <div className={poppins.className}>
+            <Component {...pageProps} />
+          </div>
+          <ToastContainer />
+        </NiceModal.Provider>
+      </SessionProvider>
+    </>
   );
 };
 
