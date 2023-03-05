7e6a7be8bb1bb0d1501892c947df5b4ed9f7fb2d
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-05 12:33:44 +0100

    add dashboard view

---
+++ src/views/Dashboard.view.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "~/components/layouts/MainLayout";
+
+export function DashboardView() {
+  return (
+    <MainLayout>
+      <div>
+        <h1>Dashboard page</h1>
+      </div>
+    </MainLayout>
+  );
+}
