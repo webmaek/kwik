9b8a6cb44cca7e54ed769207db5be33b94118af7
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-05 12:34:21 +0100

    add tabs constant

---
+++ src/utils/constants/tabs.ts
@@ -0,0 +1,11 @@
+export const TABS_LIST = [
+  { name: "Overview", value: "overview", to: "/dashboard" },
+  { name: "Payments", value: "payments", to: "/payments" },
+];
+
+export const TABS = {
+  OVERVIEW: "overview",
+  PAYMENTS: "payments",
+};
+
+export type TAB_TYPE = "overview" | "payments";
