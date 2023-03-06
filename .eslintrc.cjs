58099ea9209a6f33865320831e62706d933c0bae
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-06 08:41:09 +0100

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-    },
-  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
