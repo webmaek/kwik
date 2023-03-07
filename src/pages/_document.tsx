b26c0ce50283f28eb81d69bd3a3f3a7123ce8e16
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-07 10:42:35 +0100

    add link for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" href="/kwik.png" />
+      </Head>
       <body className="bg-gray-900 text-gray-50">
         <Main />
         <NextScript />
