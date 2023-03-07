b200d1129196956491ca9d734624ce87261c749c
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-07 10:42:09 +0100

    remove title prop

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,21 +2,19 @@ import { Tabs } from "~/components/Tabs";
 import { Header } from "~/components/Header";
 
 type MainLayoutProps = {
-  title?: string;
   showBackUrl?: boolean;
   showTabs?: boolean;
   children: React.ReactNode;
 };
 
 export function MainLayout({
-  title = "Dashboard",
   showBackUrl = false,
   showTabs = true,
   children,
 }: MainLayoutProps) {
   return (
     <div className="p-4 px-8">
-      <Header title={title} showBackUrl={showBackUrl} />
+      <Header showBackUrl={showBackUrl} />
       {showTabs && <Tabs />}
       <main>{children}</main>
     </div>
